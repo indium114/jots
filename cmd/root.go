62a15688484c495a8e10ef839b1e66d4dc9645d2
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-08 16:31:25 +0200

    chore: cobra-cli init

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"context"
+	"os"
+
+	"github.com/charmbracelet/fang"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "jots",
+	Short: "A minimal CLI journaling tool",
+}
+
+func Execute() {
+	err := fang.Execute(context.Background(), rootCmd)
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/stikypiston/jots/cmd"
+
+func main() {
+	cmd.Execute()
+}
